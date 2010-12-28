035b1cda8438dc207933dd0442c7cae64b34a1cd
Sean T Allen <sean@monkeysnatchbanana.com>
2010-12-28 16:18:13 -0500

    Basic compojure infrastructure in place.

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject to-do "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "A simple to do list using compojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.5.3"]
+                 [ring/ring-core "0.3.5"]
+                 [ring/ring-jetty-adapter "0.3.5"]])
--- src/to_do/core.clj
+++ src/to_do/core.clj
@@ -1,1 +1,15 @@
-(ns to-do.core)
+(ns to-do.core
+  [:use compojure.core
+        ring.adapter.jetty])
+
+(defn welcome []
+  "hello")
+
+(defroutes myroutes
+  (GET "/" [] (welcome)))
+
+
+(defonce server 
+  (run-jetty #'myroutes
+    { :join? true
+    :port 8080}))

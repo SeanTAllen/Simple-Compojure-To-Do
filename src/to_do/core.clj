1e1a491efa2afd180f28e991d5fed248e2154711
Sean T Allen <sean@monkeysnatchbanana.com>
2010-12-31 17:49:30 -0500

    Added basic 'add todo' functionality.

--- src/to_do/core.clj
+++ src/to_do/core.clj
@@ -1,25 +1,37 @@
 (ns to-do.core
   (:require [to-do.templates :as templates])
   [:use compojure.core
-        ring.adapter.jetty
-        ring.middleware.file])
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.file :only [wrap-file]]
+        [ring.util.response :only [redirect]]])
 
-(def *todo* [{:id 1 :title "A To Do" :completed false}
-             {:id 2 :title "Not Done" :completed false}])
+(def *todo* (ref 
+  [{:id 1 :title "A To Do" :completed false}
+   {:id 2 :title "Not Done" :completed false}]))
 
-(defn completed-todos []
-  (filter #(= (get % :completed) false) *todo*))
+(defn uncompleted-todos []
+  (filter #(= (get % :completed) false) @*todo*))
+
+(defn add-todo [todo]
+  (dosync (alter *todo* conj todo)))
+  
+(defn create-todo [title]
+  (add-todo {:id 1 :title title :completed false}))
             
 (defn todo-list []
-  (apply str(templates/todo-list (completed-todos))))
+  (apply str(templates/todo-list (uncompleted-todos))))
 
 (defn todo-new []
   (apply str(templates/todo-new)))
+  
+(defn todo-add [title]
+  (create-todo title)
+  (redirect "/"))
 
 (defroutes myroutes
   (GET "/" [] (todo-list))
   (GET "/new" [] (todo-new))
-  (POST "/add" [] (todo-list))
+  (POST "/add" [title] (todo-add title))
   (POST "/finished" [] (todo-list)))
 
 (def app 
@@ -27,4 +39,4 @@
       (wrap-file "public")))
 
 (defonce server 
-  (run-jetty #'app { :join? true :port 8080}))
+  (run-jetty #'app { :join? false :port 8080}))

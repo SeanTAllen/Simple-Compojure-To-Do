a6f626b7f3ec4b7184d1807389aecd1e2831aa33
Sean T Allen <sean@monkeysnatchbanana.com>
2010-12-30 23:21:41 -0500

    Big jump to serving a real-ish index page added.
    More 'baby steps' from here on out. ( probably ).

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.3"]
                  [ring/ring-core "0.3.5"]
-                 [ring/ring-jetty-adapter "0.3.5"]])
+                 [ring/ring-jetty-adapter "0.3.5"]
+                 [enlive "1.0.0-SNAPSHOT"]])
--- src/to_do/core.clj
+++ src/to_do/core.clj
@@ -1,9 +1,16 @@
 (ns to-do.core
+  (:require [net.cgrand.enlive-html :as html])
   [:use compojure.core
-        ring.adapter.jetty])
+        ring.adapter.jetty
+        ring.middleware.file])
 
+(def *todo* [])
+
+(html/deftemplate index "templates/list.html"
+  [context])
+          
 (defn welcome []
-  "hello")
+  (apply str(index {})))
 
 (defroutes myroutes
   (GET "/" [] (welcome))
@@ -11,8 +18,9 @@
   (POST "/add" [] (welcome)
   (POST "/finished" [] (welcome)))))
 
+(def app 
+  (-> #'myroutes
+      (wrap-file "public")))
 
 (defonce server 
-  (run-jetty #'myroutes
-    { :join? true
-    :port 8080}))
+  (run-jetty #'app { :join? true :port 8080}))

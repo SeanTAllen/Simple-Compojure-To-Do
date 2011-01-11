c4e0990439426774347ae236aa4014a9730ddefa
Sean T Allen <sean@monkeysnatchbanana.com>
2011-01-10 21:14:51 -0500

    Switched from server.clj driven jetty to using lein-ring.
    Much simpler setup.
    This switch came from a suggestion from James Reeves on the clojure-web-dev list.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject to-do "1.0.0"
+(defproject to-do "1.0.1"
   :description "A simple to do list using compojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.3"]
                  [ring/ring-core "0.3.5"]
                  [ring/ring-jetty-adapter "0.3.5"]
-                 [enlive "1.0.0-SNAPSHOT"]])
+                 [enlive "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-ring "0.2.4"]]
+  :ring {:handler to-do.core/myroutes})
--- src/to_do/server.clj
+++
@@ -1,8 +0,0 @@
-(ns to-do.server
-  [:use [to-do.core :only [app]]
-        [ring.adapter.jetty :only [run-jetty]]])
-
-(defn -main []
-  (run-jetty #'app {:port 8080}))
-  
-  
